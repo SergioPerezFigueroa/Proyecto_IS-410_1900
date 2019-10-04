7c389c97675ec5c3ebd4e567336f7d903393dc57
sergioPerezFigueroa <sergioperez7816@gmail.com>
2019-10-04 16:57:27 -0600

    Update class.Persona.php

--- Aplicacion/2-ClasesPHP5/class.Persona.php
+++ Aplicacion/2-ClasesPHP5/class.Persona.php
@@ -2,37 +2,13 @@
 
 error_reporting(E_ALL);
 
-/**
- * modelo sin título - class.Persona.php
- *
- * $Id$
- *
- * This file is part of modelo sin título.
- *
- * Automatically generated on 10.09.2019, 23:00:10 with ArgoUML PHP module 
- * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
- *
- * @author firstname and lastname of author, <author@example.org>
- */
+
 
 if (0 > version_compare(PHP_VERSION, '5')) {
     die('This file was generated for PHP 5');
 }
 
-/* user defined includes */
-// section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000866-includes begin
-// section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000866-includes end
 
-/* user defined constants */
-// section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000866-constants begin
-// section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000866-constants end
-
-/**
- * Short description of class Persona
- *
- * @access public
- * @author firstname and lastname of author, <author@example.org>
- */
 class Persona
 {
     // --- ASSOCIATIONS ---
@@ -40,245 +16,78 @@ class Persona
 
     // --- ATTRIBUTES ---
 
-    /**
-     * Short description of attribute Nombre
-     *
-     * @access private
-     * @var String
-     */
+  
     private $Nombre = null;
-
-    /**
-     * Short description of attribute Apellido
-     *
-     * @access private
-     * @var String
-     */
     private $Apellido = null;
-
-    /**
-     * Short description of attribute ID
-     *
-     * @access private
-     * @var int
-     */
     private $ID = 0;
-
-    /**
-     * Short description of attribute Edad
-     *
-     * @access private
-     * @var Int
-     */
     private $Edad = null;
-
-    /**
-     * Short description of attribute Direccion
-     *
-     * @access private
-     * @var String
-     */
     private $Direccion = null;
-
-    /**
-     * Short description of attribute Nacionalidad
-     *
-     * @access private
-     * @var String
-     */
     private $Nacionalidad = null;
-
-    /**
-     * Short description of attribute Correo
-     *
-     * @access private
-     * @var String
-     */
     private $Correo = null;
-
-    /**
-     * Short description of attribute Contraseña
-     *
-     * @access public
-     * @var String
-     */
-    public $Contraseña = null;
-
-    /**
-     * Short description of attribute Telefono
-     *
-     * @access public
-     * @var String
-     */
-    public $Telefono = null;
+    private $Password = null;
+    private $Telefono = null;
 
     // --- OPERATIONS ---
 
-    /**
-     * Short description of method setNombre
-     *
-     * @access public
-     * @author firstname and lastname of author, <author@example.org>
-     * @param  String Apellido
-     * @return String
-     */
+   
     public function setNombre( String $Apellido)
     {
         $returnValue = null;
-
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:000000000000087B begin
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:000000000000087B end
-
         return $returnValue;
     }
 
-    /**
-     * Short description of method getNombre
-     *
-     * @access public
-     * @author firstname and lastname of author, <author@example.org>
-     * @return mixed
-     */
-    public function getNombre()
-    {
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:000000000000087E begin
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:000000000000087E end
+    public function getNombre(){
+     
+    }
+    public function setEdad( String $Edad){
+        
+    }
+    
+    public function setApellido( String $Apellido){
+        
     }
 
-    /**
-     * Short description of method setApellido
-     *
-     * @access public
-     * @author firstname and lastname of author, <author@example.org>
-     * @param  String Apellido
-     * @return mixed
-     */
-    public function setApellido( String $Apellido)
-    {
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000880 begin
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000880 end
+    public function setCorreo( String $Correo){
+       
     }
 
-    /**
-     * Short description of method getApellido
-     *
-     * @access public
-     * @author firstname and lastname of author, <author@example.org>
-     * @return mixed
-     */
-    public function getApellido()
-    {
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000882 begin
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000882 end
+   
+    public function getApellido(){      
     }
 
-    /**
-     * Short description of method setID
-     *
-     * @access public
-     * @author firstname and lastname of author, <author@example.org>
-     * @param  int ID
-     * @return mixed
-     */
-    public function setID($ID)
-    {
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000884 begin
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000884 end
+   
+    public function setID($ID){
+     
     }
 
-    /**
-     * Short description of method getID
-     *
-     * @access public
-     * @author firstname and lastname of author, <author@example.org>
-     * @return mixed
-     */
-    public function getID()
-    {
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000886 begin
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000886 end
+    public function getID(){
+       
     }
 
-    /**
-     * Short description of method setCorreo
-     *
-     * @access public
-     * @author firstname and lastname of author, <author@example.org>
-     * @param  String Correo
-     * @return mixed
-     */
-    public function setCorreo( String $Correo)
-    {
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000888 begin
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000888 end
+  
+
+    public function getCorreo(){
+        
     }
 
-    /**
-     * Short description of method getCorreo
-     *
-     * @access public
-     * @author firstname and lastname of author, <author@example.org>
-     * @return mixed
-     */
-    public function getCorreo()
-    {
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000890 begin
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000890 end
+    
+  
+
+   
+    public function getEdad(){
+        
     }
 
-    /**
-     * Short description of method setEdad
-     *
-     * @access public
-     * @author firstname and lastname of author, <author@example.org>
-     * @param  String Edad
-     * @return mixed
-     */
-    public function setEdad( String $Edad)
-    {
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000892 begin
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000892 end
+   
+    public function setNacionalidad(){
+      
     }
 
-    /**
-     * Short description of method getEdad
-     *
-     * @access public
-     * @author firstname and lastname of author, <author@example.org>
-     * @return mixed
-     */
-    public function getEdad()
-    {
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000895 begin
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000895 end
+    
+    public function getNacionalidad(){
+        
     }
 
-    /**
-     * Short description of method setNacionalidad
-     *
-     * @access public
-     * @author firstname and lastname of author, <author@example.org>
-     * @return mixed
-     */
-    public function setNacionalidad()
-    {
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000897 begin
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000897 end
-    }
-
-    /**
-     * Short description of method getNacionalidad
-     *
-     * @access public
-     * @author firstname and lastname of author, <author@example.org>
-     * @return mixed
-     */
-    public function getNacionalidad()
-    {
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000899 begin
-        // section -64--88-0-16-39cce6ef:16d03080f9b:-8000:0000000000000899 end
-    }
-
-} /* end of class Persona */
+} 
 
 ?>
